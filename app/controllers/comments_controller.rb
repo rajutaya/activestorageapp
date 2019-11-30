155c52df84558737105622b821cc9d10bfec357e
rajutaya <rajutaya@gmail.com>
2019-11-30 18:32:40 +0530

    Feature: Letter pad added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,4 +16,22 @@ class CommentsController < ApplicationController
   def show
     @comment = Comment.find(params[:id])
   end
+
+  def letter_pad
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf: "letter", 
+        disposition: 'inline',
+        layout: false,
+        margin:  {   top: 0,
+                     bottom: 0,
+                     left: 0,
+                     right: 0 }
+         # footer:  { html: { template: 'comments/footer.pdf.erb',layout: false } }
+      end
+    end
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :comments
+  resources :comments do
+  	collection do
+  		get 'letter_pad'
+  	end
+  end
 end
